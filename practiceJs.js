e5fab5b08de2b7f9cac52bb55f96376f271f497c
ryanotaydemusa14 <ryanotaydemusa14@gmail.com>
2021-09-22 17:06:43 +0800

    Update practiceJs.js

--- practiceJs.js
+++ practiceJs.js
@@ -1,8 +1,9 @@
 var todos = [
-    "clean room!",
-    "brush teeth!",
-    "exercise!",
-    "study!",
-    "eat healthy!"
+  "clean room!",
+  "brush teeth!",
+  "exercise!",
+  "study!",
+  "eat healthy!",
 ];
-
+var x = 5;
+var y = 7;
